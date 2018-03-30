62566f4dbf65e1157dd9cd4c1adfb5a94789c4dd
f.gorodkovets <f.gorodkovets@a1qa.com>
2018-03-30 17:03:33 +0300

    fixes syntax by drupal standards

--- sites/all/modules/text_mistakes_reports/text_mistakes_reports_grid.tpl.php
+++ sites/all/modules/text_mistakes_reports/text_mistakes_reports_grid.tpl.php
@@ -1,34 +1,37 @@
 <div class="container">
     <table class="table table-hover table-bordered ">
         <thead>
-            <tr>
-            <?php foreach ($rows[0] as $key => $value): ?>
+        <tr>
+          <?php foreach ($rows[0] as $key => $value): ?>
 
-                <th><?php print $key?></th>
+              <th><?php print $key; ?></th>
 
-            <?php endforeach; ?>
-            </tr>
+          <?php endforeach; ?>
+        </tr>
         </thead>
 
         <tbody>
         <?php foreach ($rows as $key => $row): ?>
 
             <tr>
-            <?php foreach ($row as $key => $value): ?>
+              <?php foreach ($row as $key => $value): ?>
 
-                <?php if ($key === 'Entity Link'):?>
-                    <td><a href="<?php print $value?>"><?php print $value?></a></td>
-                <?php else:; ?>
-                    <td><?php print $value?></td>
+                <?php if ($key === 'Entity Link'): ?>
+                      <td>
+                          <a href="<?php print $value; ?>"><?php print $value; ?></a>
+                      </td>
+                <?php else: ?>
+                      <td><?php print $value; ?></td>
                 <?php endif; ?>
 
-            <?php endforeach; ?>
+              <?php endforeach; ?>
 
-                    <td>
-                        <button id="report_delete_<?php print $row['Report ID']?>" class="btn_report_delete btn">
-                            Delete
-                        </button>
-                    </td>
+                <td>
+                    <button id="report_delete_<?php print $row['Report ID']; ?>"
+                            class="btn_report_delete btn">
+                        Delete
+                    </button>
+                </td>
             </tr>
 
         <?php endforeach; ?>
--- sites/all/modules/text_mistakes_reports/text_mistakes_reports_popup.tpl.php
+++ sites/all/modules/text_mistakes_reports/text_mistakes_reports_popup.tpl.php
@@ -13,72 +13,14 @@
                 </div>
             </div>
             <div class="popup_button_panel">
-                <button id="popup_send" type="button" class="btn btn-dark">Send</button>
-                <button id="popup_hide" type="button" class="btn btn-dark">Cancel</button>
+                <button id="popup_send" type="button" class="btn btn-dark">
+                    Send
+                </button>
+                <button id="popup_hide" type="button" class="btn btn-dark">
+                    Cancel
+                </button>
             </div>
 
         </div>
     </div>
 </div>
-
-<style>
-
-    #mistakes_popup_panel{
-        display: none;
-    }
-
-    #mistakes_popup_panel .popup_backgroung{
-        width:100%;
-        min-height:100%;
-        background-color: rgba(0,0,0,0.5);
-        overflow:hidden;
-        position:fixed;
-        top:0px;
-        left: 0px;
-        z-index: 100;
-    }
-
-    #mistakes_popup_panel .popup_backgroung .popup_content{
-        margin:15% auto 0px auto;
-        width:600px;
-        min-height: 200px;
-        padding:10px;
-        background-color: #c5c5c5;
-        border-radius:5px;
-        box-shadow: 0px 0px 10px #000;
-        display: flex;
-        justify-content: flex-start;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    #mistakes_popup_panel .popup_text_container{
-        width: 100%;
-        padding: 5px;
-    }
-
-    #mistakes_popup_panel .popup_text_container .popup_text_container_title{
-        margin-left: 10px;
-    }
-
-    #mistakes_popup_panel .popup_text_container .mistakes_popup_text{
-        background-color: white;
-        border: black solid 1px;
-        border-radius: 10px;
-        padding: 5px;
-        margin: 5px;
-        display: block;
-    }
-
-    #mistakes_popup_panel .popup_button_panel {
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        padding: 10px;
-    }
-
-
-
-</style>
