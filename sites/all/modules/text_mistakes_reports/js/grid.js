f9e0eda455a76159272a44706668f16c67e56f23
f.gorodkovets <f.gorodkovets@a1qa.com>
2018-03-30 15:10:52 +0300

    fixes syntax and names

--- sites/all/modules/text_mistakes_reports/js/grid.js
+++ sites/all/modules/text_mistakes_reports/js/grid.js
@@ -1,23 +1,19 @@
 (function ($) {
     $(function() {
 
-        $('.btn_report_delete').click(function () {
-
-            reportId = $(this).attr('id').split('_')[2];
-
-            console.log(reportId);
+        $('.btn_report_delete').click( function () {
+            var reportId = $(this).attr('id').split('_')[2];
 
             $.ajax({
                 url: Drupal.settings.textMistakesReportsAjax.gridDeleteUrl,
                 type: 'POST',
-                data: {
-                    'reportId': reportId
-                },
+                data: {'reportId': reportId},
                 dataType: 'JSON',
                 success: function(data) {
                     location.reload();
                 }
             });
         });
+
     })
 }(jQuery));
--- sites/all/modules/text_mistakes_reports/js/popup.js
+++ sites/all/modules/text_mistakes_reports/js/popup.js
@@ -1,5 +1,5 @@
 (function ($) {
-    $(function() {
+    $(function () {
 
         var selectedField;
         var selectedText;
@@ -18,21 +18,21 @@
         }
 
         function selectionIsInAllowedField(currentSelection) {
-            activeFields = Drupal.settings.textMistakesReportsFields;
-            var correct = false;
-            $.each(activeFields, function( index, field ) {
-                if (!correct) {
+            var activeFields = Drupal.settings.textMistakesReportsFields;
+            var allowed = false;
+            $.each(activeFields, function (index, field) {
+                if (!allowed) {
                     var htmlName = getHtmlFieldName(field.fieldName);
 
                     if (selectionIsInSingleField(currentSelection, htmlName)) {
                         selectedField = field;
-                        correct = true;
+                        allowed = true;
                         return true;
                     }
                 }
             });
 
-            return correct;
+            return allowed;
         }
 
         function getHtmlFieldName(fieldname) {
@@ -54,7 +54,7 @@
         function selectionIsValid(selectionText) {
             if (selectionText.length > selectedField.maxLength) {
                 var text = 'selected text is too big';
-                var report_current = ' ( current - ' + selectionText.length ;
+                var report_current = ' ( current - ' + selectionText.length;
                 var report_max = ', max - ' + selectedField.maxLength + ')';
                 alert(text + report_current + report_max);
                 return false;
@@ -78,7 +78,7 @@
                 }
             }
 
-            if(count_space(selectedText) < 2) {
+            if (count_space(selectedText) < 2) {
                 var select = window.getSelection().getRangeAt(0);
                 var textInLeft = select.startContainer;
                 var startposition = select.startOffset;
@@ -89,27 +89,27 @@
                 return {
                     'text': selectedText,
                     'length': selectedText.length,
-                    'textLeft' : wordInLeft,
-                    'textRight' : wordInRight
+                    'textLeft': wordInLeft,
+                    'textRight': wordInRight
                 };
             }
 
             return {
                 'text': selectedText,
                 'length': selectedText.length,
-                'textLeft' : '',
-                'textRight' : ''
+                'textLeft': '',
+                'textRight': ''
             };
         }
 
         function getWordsInLeft(text, leftPosition, amountWords) {
             var res = 0;
             var word = '';
-            for(i = leftPosition; i >= 0; i--){
-                if(text.charAt(i) === " "){
+            for (var i = leftPosition; i >= 0; i--) {
+                if (text.charAt(i) === " ") {
                     res++;
                 }
-                if(res === amountWords){
+                if (res === amountWords) {
                     break;
                 }
                 word = text.charAt(i) + word;
@@ -120,11 +120,11 @@
         function getWordsInRight(text, rightPosition, amountWords) {
             var res = 0;
             var word = '';
-            for(i = rightPosition; i < text.length; i++){
-                if(text.charAt(i) === " "){
+            for (var i = rightPosition; i < text.length; i++) {
+                if (text.charAt(i) === " ") {
                     res++;
                 }
-                if(res === amountWords){
+                if (res === amountWords) {
                     break;
                 }
                 word += text.charAt(i);
@@ -133,10 +133,10 @@
         }
 
         function count_space(str) {
-            count = str.length;
+            var count = str.length;
             var res = 0;
-            for(i = 0; i < count; i++){
-                if(str.charAt(i) === " "){
+            for (var i = 0; i < count; i++) {
+                if (str.charAt(i) === " ") {
                     res++;
                 }
             }
@@ -147,12 +147,12 @@
             return window.getSelection().getRangeAt(0);
         }
 
-        $('body').keydown(function(e, byScript) {
+        $('body').keydown(function (e, byScript) {
             var keyCode = e.keyCode || e.charCode || e.which;
             if (keyCode === 10 || keyCode === 13) {
                 if (e.ctrlKey) {
                     var selectionInfo = getSelectionInfo();
-                    selectionInfo['text'] = selectionInfo['text'].replace(/<[^>]+>/g,'');
+                    selectionInfo['text'] = selectionInfo['text'].replace(/<[^>]+>/g, '');
 
                     if (selectionInfo['text']) {
                         var currentSelection = getSelection();
@@ -185,30 +185,22 @@
 
                 return false;
             }
-        });
+        })
+            .click(function (event) {
+                if ($(event.target).closest('#mistakes_popup_content').length === 0) {
+                    PopUpHide();
+                }
+            });
 
-        $('#popup_hide').click(function (el) {
+        $('#popup_hide').click(function () {
             PopUpHide();
         });
 
-        $('body').click(function (event) {
-            if ($(event.target).closest('#mistakes_popup_content').length === 0) {
-                PopUpHide();
-            }
-        });
-
         $('#popup_send').click(function () {
-            objectLink = $('head').find('link[rel="shortlink"]').attr('href');
-            objectType = objectLink.split('/')[1];
-            objectId = objectLink.split('/')[2];
-            console.log({
-                'objectType': objectType,
-                'objectId': objectId,
-                'objectLink': objectLink,
-                'fieldName': selectedField.fieldName,
-                'selectedText': selectedText,
-                'username': Drupal.settings.textMistakesReportsUser.username
-            });
+            var objectLink = $('head').find('link[rel="shortlink"]').attr('href');
+            var objectType = objectLink.split('/')[1];
+            var objectId = objectLink.split('/')[2];
+
             $.ajax({
                 url: Drupal.settings.textMistakesReportsAjax.sendReportUrl,
                 type: 'POST',
@@ -221,8 +213,11 @@
                     'username': Drupal.settings.textMistakesReportsUser.username
                 },
                 dataType: 'JSON',
-                success: function(data) {
-                    console.log(data);
+                success: function (data) {
+                    data = $.parseJSON(data);
+                    if (data['condition'] === 'failure') {
+                        alert(data['errorText']);
+                    }
                 }
             });
             PopUpHide();
