6bf3287da801f190ad662755a332f6446ddc32e8
f.gorodkovets <f.gorodkovets@a1qa.com>
2018-04-03 12:14:56 +0300

    - style fixes

--- sites/all/modules/text_mistakes_reports/text_mistakes_reports_popup.tpl.php
+++ sites/all/modules/text_mistakes_reports/text_mistakes_reports_popup.tpl.php
@@ -2,20 +2,20 @@
     <div class="popup_backgroung" id="mistakes_popup_background">
         <div class="popup_content" id="mistakes_popup_content">
             <h3 id="popup_title">
-              <?php print t('Popup Title') ?>
+              <?php print t('Popup Title'); ?>
             </h3>
             <div class="popup_text_container">
-                <span class="popup_text_container_title"><strong>Selected Text:</strong></span>
+                <span class="popup_text_container_title"><strong><?php print t('Selected Text:'); ?></strong></span>
                 <div class="mistakes_popup_text">
-                  <?php print t('Selected Text') ?>
+                  <?php print t('Selected Text'); ?>
                 </div>
             </div>
             <div class="popup_button_panel">
                 <button id="popup_send" type="button" class="btn btn-dark">
-                  <?php print t('Send') ?>
+                  <?php print t('Send'); ?>
                 </button>
                 <button id="popup_hide" type="button" class="btn btn-dark">
-                  <?php print t('Cancel') ?>
+                  <?php print t('Cancel'); ?>
                 </button>
             </div>
 
