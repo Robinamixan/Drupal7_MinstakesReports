783b0c6f2c030b50c9cacca36c685413ab5b8b6d
f.gorodkovets <f.gorodkovets@a1qa.com>
2018-04-03 12:28:13 +0300

    - style fixes in js

--- sites/all/modules/text_mistakes_reports/js/grid.js
+++ sites/all/modules/text_mistakes_reports/js/grid.js
@@ -1,87 +1,87 @@
 (function ($) {
-    $(function() {
+  $(function () {
 
-        var $currentPage = 0;
-        var $maxPage = Drupal.settings.textMistakesReportsAjax.gridMaxPage;
+    var currentPage = 0;
+    var maxPage = Drupal.settings.textMistakesReportsAjax.gridMaxPage;
 
-        function sendAjaxDelete($element) {
-            var $reportId = $($element).attr('id').split('_')[2];
-            var $container = $($element).parents('#report_mistake_grid').parent();
-            $.ajax({
-                url: Drupal.settings.textMistakesReportsAjax.gridDeleteUrl,
-                type: 'POST',
-                data: {
-                    'reportId': $reportId,
-                    'currentPage': $currentPage
-                },
-                dataType: 'JSON',
-                success: function($data) {
-                    $data = $.parseJSON($data);
-                    $($container).html($data['htmlGrid']);
-                    $maxPage = $data['maxPage'];
-                    $currentPage = $data['currentPage'];
+    function sendAjaxDelete($element) {
+      var reportId = $($element).attr('id').split('_')[2];
+      var $container = $($element).parents('#report_mistake_grid').parent();
+      $.ajax({
+        url: Drupal.settings.textMistakesReportsAjax.gridDeleteUrl,
+        type: 'POST',
+        data: {
+          'reportId': reportId,
+          'currentPage': currentPage
+        },
+        dataType: 'JSON',
+        success: function (data) {
+          data = $.parseJSON(data);
+          $($container).html(data['htmlGrid']);
+          maxPage = data['maxPage'];
+          currentPage = data['currentPage'];
 
-                }
-            });
         }
+      });
+    }
 
-        function sendAjaxGetNextPage($element) {
-            var $container = $($element).parents('#report_mistake_grid').parent();
+    function sendAjaxGetNextPage($element) {
+      var $container = $($element).parents('#report_mistake_grid').parent();
 
-            if (($currentPage + 1) < $maxPage) {
-                $.ajax({
-                    url: Drupal.settings.textMistakesReportsAjax.gridPageUrl,
-                    type: 'POST',
-                    data: {
-                        'currentPage': $currentPage + 1
-                    },
-                    dataType: 'JSON',
-                    success: function($data) {
-                        $data = $.parseJSON($data);
-                        $container = $($container);
-                        $container.html($data['htmlGrid']);
-                        $maxPage = $data['maxPage'];
+      if ((currentPage + 1) < maxPage) {
+        $.ajax({
+          url: Drupal.settings.textMistakesReportsAjax.gridPageUrl,
+          type: 'POST',
+          data: {
+            'currentPage': currentPage + 1
+          },
+          dataType: 'JSON',
+          success: function (data) {
+            data = $.parseJSON(data);
+            $container = $($container);
+            $container.html(data['htmlGrid']);
+            maxPage = data['maxPage'];
 
-                        $currentPage++;
-                    }
-                });
-            }
-        }
-
-        function sendAjaxGetBackPage($element) {
-            var $container = $($element).parents('#report_mistake_grid').parent();
-
-            if($currentPage > 0) {
-                $.ajax({
-                    url: Drupal.settings.textMistakesReportsAjax.gridPageUrl,
-                    type: 'POST',
-                    data: {
-                        'currentPage': $currentPage - 1
-                    },
-                    dataType: 'JSON',
-                    success: function($data) {
-                        $data = $.parseJSON($data);
-                        $container = $($container);
-                        $container.html($data['htmlGrid']);
-                        $maxPage = $data['maxPage'];
-
-                        $currentPage--;
-                    }
-                });
-            }
-        }
-
-        $(document).delegate('.btn_report_delete', 'click', function () {
-            sendAjaxDelete(this);
+            currentPage++;
+          }
         });
+      }
+    }
 
-        $(document).delegate('#grid_btn_next_page', 'click', function () {
-            sendAjaxGetNextPage(this);
+    function sendAjaxGetBackPage($element) {
+      var $container = $($element).parents('#report_mistake_grid').parent();
+
+      if (currentPage > 0) {
+        $.ajax({
+          url: Drupal.settings.textMistakesReportsAjax.gridPageUrl,
+          type: 'POST',
+          data: {
+            'currentPage': currentPage - 1
+          },
+          dataType: 'JSON',
+          success: function ($data) {
+            $data = $.parseJSON($data);
+            $container = $($container);
+            $container.html($data['htmlGrid']);
+            maxPage = $data['maxPage'];
+
+            currentPage--;
+          }
         });
+      }
+    }
 
-        $(document).delegate('#grid_btn_back_page', 'click', function () {
-            sendAjaxGetBackPage(this);
-        });
+    $(document).delegate('.btn_report_delete', 'click', function () {
+      sendAjaxDelete(this);
+    });
 
-    })
+    $(document).delegate('#grid_btn_next_page', 'click', function () {
+      sendAjaxGetNextPage(this);
+    });
+
+    $(document).delegate('#grid_btn_back_page', 'click', function () {
+      sendAjaxGetBackPage(this);
+    });
+
+  })
 }(jQuery));
--- sites/all/modules/text_mistakes_reports/js/popup.js
+++ sites/all/modules/text_mistakes_reports/js/popup.js
@@ -1,226 +1,238 @@
 (function ($) {
-    $(function () {
+  $(function () {
 
-        var selectedField;
-        var selectedText;
+    var selectedField;
+    var selectedText;
 
-        function PopUpHide() {
-            $('#mistakes_popup_panel').hide();
-        }
+    function PopUpHide() {
+      $('#mistakes_popup_panel').hide();
+    }
 
-        function PopUpShow() {
-            $('#mistakes_popup_panel').show();
-        }
+    function PopUpShow() {
+      $('#mistakes_popup_panel').show();
+    }
 
-        function selectionIsInSingleField(currentSelection, fieldClassName) {
-            return !((!$(currentSelection.startContainer.parentElement).parents(fieldClassName).length) ||
-                (!$(currentSelection.endContainer.parentElement).parents(fieldClassName).length));
-        }
+    function selectionIsInSingleField(currentSelection, fieldClassName) {
+      return !((!$(currentSelection.startContainer.parentElement).parents(fieldClassName).length) ||
+          (!$(currentSelection.endContainer.parentElement).parents(fieldClassName).length));
+    }
 
-        function selectionIsInAllowedField(currentSelection) {
-            var activeFields = Drupal.settings.textMistakesReportsFields;
-            var allowed = false;
-            $.each(activeFields, function (index, field) {
-                if (!allowed) {
-                    var htmlName = getHtmlFieldName(field.fieldName);
+    function selectionIsInAllowedField(currentSelection) {
+      var activeFields = Drupal.settings.textMistakesReportsFields;
+      var allowed = false;
+      var htmlName;
 
-                    if (selectionIsInSingleField(currentSelection, htmlName)) {
-                        selectedField = field;
-                        allowed = true;
-                        return true;
-                    }
-                }
-            });
+      $.each(activeFields, function (index, field) {
+        if (!allowed) {
+          htmlName = getHtmlFieldName(field.fieldName);
 
-            return allowed;
-        }
-
-        function getHtmlFieldName(fieldname) {
-            return '.field-name-' + fieldname.split('_').join('-');
-        }
-
-        function selectionHasValidLocation(currentSelection) {
-            if (!selectionIsInSingleField(currentSelection, '.field')) {
-                return false;
-            }
-
-            if (!selectionIsInAllowedField(currentSelection)) {
-                return false;
-            }
-
+          if (selectionIsInSingleField(currentSelection, htmlName)) {
+            selectedField = field;
+            allowed = true;
             return true;
+          }
+        }
+      });
+
+      return allowed;
+    }
+
+    function getHtmlFieldName(fieldname) {
+      return '.field-name-' + fieldname.split('_').join('-');
+    }
+
+    function selectionHasValidLocation(currentSelection) {
+      if (!selectionIsInSingleField(currentSelection, '.field')) {
+        return false;
+      }
+
+      if (!selectionIsInAllowedField(currentSelection)) {
+        return false;
+      }
+
+      return true;
+    }
+
+    function selectionIsValid(selectionText) {
+      if (selectionText.length > selectedField.maxLength) {
+        var text = 'selected text is too big';
+        var report_current = ' ( current - ' + selectionText.length;
+        var report_max = ', max - ' + selectedField.maxLength + ')';
+        alert(text + report_current + report_max);
+        return false;
+      }
+
+      return true;
+    }
+
+    function getSelectionInfo() {
+      var selectedText = '';
+      var $container;
+      if (typeof window.getSelection !== 'undefined') {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+          if (sel.baseNode.parentNode.id !== 'content') {
+            $container = document.createElement('div');
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+              $container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            selectedText = $container.innerHTML;
+          }
+        }
+      }
+
+      if (count_space(selectedText) < 2) {
+        var select = window.getSelection().getRangeAt(0);
+        var textInLeft = select.startContainer;
+        var startposition = select.startOffset;
+        var textInRight = select.endContainer;
+        var endposition = select.endOffset;
+        var wordInLeft = getWordsInLeft($(textInLeft).text(), startposition - 1, 3);
+        var wordInRight = getWordsInRight($(textInRight).text(), endposition, 3);
+        return {
+          'text': selectedText,
+          'length': selectedText.length,
+          'textLeft': wordInLeft,
+          'textRight': wordInRight
+        };
+      }
+
+      return {
+        'text': selectedText,
+        'length': selectedText.length,
+        'textLeft': '',
+        'textRight': ''
+      };
+    }
+
+    function getWordsInLeft(text, leftPosition, amountWords) {
+      var res = 0;
+      var word = '';
+      for (var i = leftPosition; i >= 0; i--) {
+        if (text.charAt(i) === ' ') {
+          res++;
+        }
+        if (res === amountWords) {
+          break;
+        }
+        word = text.charAt(i) + word;
+      }
+      return word;
+    }
+
+    function getWordsInRight(text, rightPosition, amountWords) {
+      var res = 0;
+      var word = '';
+      for (var i = rightPosition; i < text.length; i++) {
+        if (text.charAt(i) === ' ') {
+          res++;
+        }
+        if (res === amountWords) {
+          break;
+        }
+        word += text.charAt(i);
+      }
+      return word;
+    }
+
+    function count_space(str) {
+      var count = str.length;
+      var res = 0;
+      for (var i = 0; i < count; i++) {
+        if (str.charAt(i) === ' ') {
+          res++;
+        }
+      }
+      return res;
+    }
+
+    function getSelection() {
+      return window.getSelection().getRangeAt(0);
+    }
+
+    $('body').keydown(function (e, byScript) {
+      var keyCode = e.keyCode || e.charCode || e.which;
+      var selectionInfo;
+      var currentSelection;
+      var mainText;
+      var inLeftText;
+      var inRightText;
+
+      if (keyCode === 10 || keyCode === 13) {
+        if (e.ctrlKey) {
+          selectionInfo = getSelectionInfo();
+          selectionInfo['text'] = selectionInfo['text'].replace(/<[^>]+>/g, '');
+
+          if (selectionInfo['text']) {
+            currentSelection = getSelection();
+
+            if (selectionHasValidLocation(currentSelection)) {
+              if (selectionIsValid(selectionInfo['text'])) {
+
+                if (selectionInfo['textLeft'] !== '') {
+                  mainText = '<span style="text-decoration: underline;">' + selectionInfo['text'] + '</span>';
+                  inLeftText = selectionInfo['textLeft'];
+                  inRightText = selectionInfo['textRight'];
+                  selectedText = inLeftText + selectionInfo['text'] + inRightText;
+                  $('.mistakes_popup_text').html(inLeftText + mainText + inRightText);
+                }
+                else {
+                  selectedText = selectionInfo['text'];
+                  $('.mistakes_popup_text').html(selectionInfo['text']);
+                }
+
+                $('#popup_title').text(selectedField['reportText']);
+
+                PopUpShow();
+              }
+            }
+            else {
+              alert('wrong selection!(you cant select this text)');
+            }
+          }
+          else {
+            alert('you didn\'t select anything!');
+          }
         }
 
-        function selectionIsValid(selectionText) {
-            if (selectionText.length > selectedField.maxLength) {
-                var text = 'selected text is too big';
-                var report_current = ' ( current - ' + selectionText.length;
-                var report_max = ', max - ' + selectedField.maxLength + ')';
-                alert(text + report_current + report_max);
-                return false;
-            }
-
-            return true;
-        }
-
-        function getSelectionInfo() {
-            var selectedText = '';
-            if (typeof window.getSelection !== 'undefined') {
-                var sel = window.getSelection();
-                if (sel.rangeCount) {
-                    if (sel.baseNode.parentNode.id !== 'content') {
-                        var container = document.createElement('div');
-                        for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                            container.appendChild(sel.getRangeAt(i).cloneContents());
-                        }
-                        selectedText = container.innerHTML;
-                    }
-                }
-            }
-
-            if (count_space(selectedText) < 2) {
-                var select = window.getSelection().getRangeAt(0);
-                var textInLeft = select.startContainer;
-                var startposition = select.startOffset;
-                var textInRight = select.endContainer;
-                var endposition = select.endOffset;
-                var wordInLeft = getWordsInLeft($(textInLeft).text(), startposition - 1, 3);
-                var wordInRight = getWordsInRight($(textInRight).text(), endposition, 3);
-                return {
-                    'text': selectedText,
-                    'length': selectedText.length,
-                    'textLeft': wordInLeft,
-                    'textRight': wordInRight
-                };
-            }
-
-            return {
-                'text': selectedText,
-                'length': selectedText.length,
-                'textLeft': '',
-                'textRight': ''
-            };
-        }
-
-        function getWordsInLeft(text, leftPosition, amountWords) {
-            var res = 0;
-            var word = '';
-            for (var i = leftPosition; i >= 0; i--) {
-                if (text.charAt(i) === " ") {
-                    res++;
-                }
-                if (res === amountWords) {
-                    break;
-                }
-                word = text.charAt(i) + word;
-            }
-            return word;
-        }
-
-        function getWordsInRight(text, rightPosition, amountWords) {
-            var res = 0;
-            var word = '';
-            for (var i = rightPosition; i < text.length; i++) {
-                if (text.charAt(i) === " ") {
-                    res++;
-                }
-                if (res === amountWords) {
-                    break;
-                }
-                word += text.charAt(i);
-            }
-            return word;
-        }
-
-        function count_space(str) {
-            var count = str.length;
-            var res = 0;
-            for (var i = 0; i < count; i++) {
-                if (str.charAt(i) === " ") {
-                    res++;
-                }
-            }
-            return res;
-        }
-
-        function getSelection() {
-            return window.getSelection().getRangeAt(0);
-        }
-
-        $('body').keydown(function (e, byScript) {
-            var keyCode = e.keyCode || e.charCode || e.which;
-            if (keyCode === 10 || keyCode === 13) {
-                if (e.ctrlKey) {
-                    var selectionInfo = getSelectionInfo();
-                    selectionInfo['text'] = selectionInfo['text'].replace(/<[^>]+>/g, '');
-
-                    if (selectionInfo['text']) {
-                        var currentSelection = getSelection();
-
-                        if (selectionHasValidLocation(currentSelection)) {
-                            if (selectionIsValid(selectionInfo['text'])) {
-
-                                if (selectionInfo['textLeft'] !== '') {
-                                    var mainText = '<span style="text-decoration: underline;">' + selectionInfo['text'] + '</span>';
-                                    var leftText = selectionInfo['textLeft'];
-                                    var rightText = selectionInfo['textRight'];
-                                    selectedText = leftText + selectionInfo['text'] + rightText;
-                                    $('.mistakes_popup_text').html(leftText + mainText + rightText);
-                                } else {
-                                    selectedText = selectionInfo['text'];
-                                    $('.mistakes_popup_text').html(selectionInfo['text']);
-                                }
-
-                                $('#popup_title').text(selectedField['reportText']);
-
-                                PopUpShow();
-                            }
-                        } else {
-                            alert('wrong selection!(you cant select this text)');
-                        }
-                    } else {
-                        alert('you didn\'t select anything!');
-                    }
-                }
-
-                return false;
-            }
-        })
-            .click(function (event) {
-                if ($(event.target).closest('#mistakes_popup_content').length === 0) {
-                    PopUpHide();
-                }
-            });
-
-        $('#popup_hide').click(function () {
-            PopUpHide();
-        });
-
-        $('#popup_send').click(function () {
-            var objectLink = $('head').find('link[rel="shortlink"]').attr('href');
-            var objectType = objectLink.split('/')[1];
-            var objectId = objectLink.split('/')[2];
-
-            $.ajax({
-                url: Drupal.settings.textMistakesReportsAjax.sendReportUrl,
-                type: 'POST',
-                data: {
-                    'objectType': objectType,
-                    'objectId': objectId,
-                    'objectLink': objectLink,
-                    'fieldName': selectedField.fieldName,
-                    'selectedText': selectedText,
-                    'userId': Drupal.settings.textMistakesReportsUser.userId
-                },
-                dataType: 'JSON',
-                success: function (data) {
-                    data = $.parseJSON(data);
-                    if (data['condition'] === 'failure') {
-                        alert(data['errorText']);
-                    }
-                }
-            });
-            PopUpHide();
-        });
+        return false;
+      }
     })
+        .click(function (event) {
+          if ($(event.target).closest('#mistakes_popup_content').length === 0) {
+            PopUpHide();
+          }
+        });
+
+    $('#popup_hide').click(function () {
+      PopUpHide();
+    });
+
+    $('#popup_send').click(function () {
+      var objectLink = $('head').find('link[rel="shortlink"]').attr('href');
+      var objectType = objectLink.split('/')[1];
+      var objectId = objectLink.split('/')[2];
+
+      $.ajax({
+        url: Drupal.settings.textMistakesReportsAjax.sendReportUrl,
+        type: 'POST',
+        data: {
+          'objectType': objectType,
+          'objectId': objectId,
+          'objectLink': objectLink,
+          'fieldName': selectedField.fieldName,
+          'selectedText': selectedText,
+          'userId': Drupal.settings.textMistakesReportsUser.userId
+        },
+        dataType: 'JSON',
+        success: function (data) {
+          data = $.parseJSON(data);
+          if (data['condition'] === 'failure') {
+            alert(data['errorText']);
+          }
+        }
+      });
+      PopUpHide();
+    });
+  })
 }(jQuery));
